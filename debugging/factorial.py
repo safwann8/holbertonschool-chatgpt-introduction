88de4b33251bbc80e237b3d016c3e042fa441a71
safwann8 <safoulefou83@gmail.com>
2026-01-15 09:54:29 +0100

    fix: correct indentation and update file permissions for factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
+import sys
+
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-    n -= 1
+        n -= 1
+
     return result
 
 f = factorial(int(sys.argv[1]))
